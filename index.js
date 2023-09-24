4907bf7ce08d7036773a1af45e730f371def849a
Santiago Feijoo <santiago.feijoo96@gmail.com>
2023-09-24 04:11:10 -0300

    overflow hidden para evitar el scrolleo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById("menu");
 const nav = document.getElementById("nav");
 const closeMenu = document.getElementById("close");
+const body = document.querySelector(".body");
 menu.addEventListener("click", () => {
   /* 
         1- mostrar el menu desplegable
@@ -11,10 +12,12 @@ menu.addEventListener("click", () => {
   nav.classList.toggle("nav--visible");
   menu.classList.toggle("menu--visible");
   closeMenu.classList.toggle("close-menu--visible");
+  body.classList.toggle("body-no-scroll");
 });
 
 closeMenu.addEventListener("click", () => {
   nav.classList.toggle("nav--visible");
   menu.classList.toggle("menu--visible");
   closeMenu.classList.toggle("close-menu--visible");
+  body.classList.toggle("body-no-scroll");
 });
